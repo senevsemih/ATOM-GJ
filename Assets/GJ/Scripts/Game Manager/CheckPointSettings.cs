3b0cbc7f0f2ba972483dc663a620dadf92b1556d
senevsemih <senevsemih@gmail.com>
2021-02-14 17:27:57 +0300

    +added : checkpoint script

---
+++ Assets/GJ/Scripts/Game Manager/CheckPointSettings.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Assets.GJ.Scripts.Game_Manager
+{
+    [CreateAssetMenu(fileName = "New CheckPoint Settings", menuName = "CheckPoint/CheckPoint Settings", order = 8)]
+    public class CheckPointSettings : ScriptableObject
+    {
+        [SerializeField] private Vector3 _level1CheckPoint;
+        
+        public Vector3 Level1CheckPoint => _level1CheckPoint;
+    }
+}
