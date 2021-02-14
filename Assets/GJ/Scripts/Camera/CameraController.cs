56a963866fe923c100c5a35c39e98fceb300b412
senevsemih <senevsemih@gmail.com>
2021-02-14 17:36:30 +0300

    +added : camera script

---
+++ Assets/GJ/Scripts/Camera/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Assets.GJ.Scripts.Camera
+{
+    public class CameraController : MonoBehaviour
+    {
+        [SerializeField] private CameraSettings _cameraSettings;
+        [SerializeField] private Transform _character;
+        [SerializeField] private Transform _camera;
+        
+        private void Update()
+        {
+            CameraPositionFollow();
+        }
+        
+        private void CameraPositionFollow()
+        {
+            _camera.position = Vector3.Lerp(_camera.position,
+                _character.position + _cameraSettings.Offset, 
+                Time.deltaTime * _cameraSettings.PositionLerpSpeed);
+        }
+    }
+}
---
+++ Assets/GJ/Scripts/Camera/CameraSettings.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Assets.GJ.Scripts.Camera
+{
+    [CreateAssetMenu(fileName = "New Camera Settings", menuName = "Camera/CameraSettings", order = 5)]
+    public class CameraSettings : ScriptableObject
+    {
+        [SerializeField] private Vector3 _offset;
+        [SerializeField] private float _positionLerpSpeed = 1f;
+
+        public Vector3 Offset => _offset;
+        public float PositionLerpSpeed => _positionLerpSpeed;
+    }
+}
