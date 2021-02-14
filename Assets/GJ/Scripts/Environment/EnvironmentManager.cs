3a70a684e0dd68fffa884e9e44486e5177cff9ae
senevsemih <senevsemih@gmail.com>
2021-02-14 17:35:20 +0300

    +added : environment script

---
+++ Assets/GJ/Scripts/Environment/EnvironmentManager.cs
@@ -0,0 +1,45 @@
+﻿using Assets.GJ.Scripts.Collectable;
+using UnityEngine;
+
+namespace Assets.GJ.Scripts.Environment
+{
+    public class EnvironmentManager : MonoBehaviour
+    {
+        [SerializeField] private EnvironmentSettings _environmentSettings;
+        [SerializeField] private CollectableSettings _collectableSettings;
+        
+        private Material _button;
+        private bool _hasKey;
+
+        private bool _elevatorActive;
+
+        private void Awake()
+        {
+            _button = GetComponent<MeshRenderer>().material;
+        }
+
+        private void ActiveToElevator()
+        {
+            if (_collectableSettings.Key > 0)
+            {
+                _button.color = _environmentSettings.ElevatorButtonColor;
+                _hasKey = true;
+                _elevatorActive = false;
+            }
+        }
+
+        private void OnTriggerStay(Collider other)
+        {
+            ActiveToElevator();
+            
+            bool hit = other.gameObject.CompareTag("Main Character");
+            if (hit && _hasKey)
+            {
+                if (Input.GetKeyDown(KeyCode.C) && !_elevatorActive)
+                {
+                    _elevatorActive = true;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/GJ/Scripts/Environment/EnvironmentSettings.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Assets.GJ.Scripts.Environment
+{
+    [CreateAssetMenu(fileName = "New Environment Settings", menuName = "Environment/Environment Settings", order = 7)]
+    public class EnvironmentSettings : ScriptableObject
+    {
+        [Header("ELEVATOR BUTTON")] 
+        [SerializeField] private Color _elevatorButtonColor;
+        public Color ElevatorButtonColor => _elevatorButtonColor;
+    }
+}
