da2092de864bd85a9e71ab51a257186e7d651864
senevsemih <senevsemih@gmail.com>
2021-02-14 17:35:48 +0300

    +added : collectable script

---
+++ Assets/GJ/Scripts/Collectable/CollectableManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.GJ.Scripts.Collectable
+{
+    public class CollectableManager : MonoBehaviour
+    {
+        [SerializeField] private CollectableSettings _collectableSettings;
+        [SerializeField] private Image _keyImage;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            bool hit = other.gameObject.CompareTag("Main Character");
+            if (!hit) return;
+            _collectableSettings.Key += 1;
+            _keyImage.enabled = true;
+            
+            Destroy(gameObject, .1f);
+        }
+    }
+}
---
+++ Assets/GJ/Scripts/Collectable/CollectableSettings.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Assets.GJ.Scripts.Collectable
+{
+    [CreateAssetMenu(fileName = "New Collectable Settings", menuName = "Collectable/Collectable Settings", order = 6)]
+    public class CollectableSettings : ScriptableObject
+    {
+        [SerializeField] private int _key = 0;
+
+        public int Key
+        {
+            get => _key;
+            set => _key = value;
+        }
+    }
+}
