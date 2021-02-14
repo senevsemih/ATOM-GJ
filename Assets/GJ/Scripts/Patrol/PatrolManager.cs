6d6c552fd9478395c7ade17213282be29e86705c
senevsemih <senevsemih@gmail.com>
2021-02-14 17:27:17 +0300

    +added : enemy patrol direction

---
+++ Assets/GJ/Scripts/Patrol/PatrolManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Assets.GJ.Scripts.Patrol
+{
+    public class PatrolManager : MonoBehaviour
+    {
+        [SerializeField] private PatrolSettings _patrolSettings;
+        private Vector3 direction;
+
+        public Vector3 Direction => direction;
+
+        private void Start()
+        {
+            direction = _patrolSettings.Direciton;
+        }
+    }
+}
---
+++ Assets/GJ/Scripts/Patrol/PatrolSettings.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Assets.GJ.Scripts.Patrol
+{
+    [CreateAssetMenu(fileName = "New Patrol Settings", menuName = "Patrol/Patrol Settings", order = 2)]
+    public class PatrolSettings : ScriptableObject
+    {
+        [SerializeField] private Vector3 _direciton;
+
+        public Vector3 Direciton => _direciton;
+    }
+}
